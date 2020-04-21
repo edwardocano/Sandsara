54ae40900aef3a99128484686121a163e108d1b8
AluUriel Ubuntu <alu.uriel.gp@gmail.com>
2020-04-21 17:36:58 -0500

    protocolo de comunicacion implementado

--- blueTest/blueTest.ino
+++ blueTest/blueTest.ino
@@ -10,57 +10,123 @@ void setup() {
   Serial.begin(115200);
   SerialBT.begin("ESP32test"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
+  delay(1000);
 }
 
+int codeError, indexWord, bytesToRead;
+uint8_t dataBt[10000];
+String line;
+String fileNameBt;
+
 void loop() {
-    int codeError, index, bytesToRead;
-    uint8_t dataBt[10000];
-    String line;
+    //Serial.println("antes de bt available");
     if (SerialBT.available()){
-        CodeError = readLine(line);
+        codeError = readLine(line);
         if (line.equals("transferir")){
-            while(true){
-                SerialBT.write("ok");
-                CodeError = readLine(line);
-                index = line.indexOf("bytes");
-                if (index != -1 ){
-                    bytesToRead = int(line.substring(5) );
-                    CodeError = readBt(dataBt, bytesToRead);
-                    for (int i = 0; i < bytesToRead; i++){
-                        serial.print(dataBt[i]);
+            SerialBT.println("ok");
+            codeError = readLine(line);
+            if (codeError != 0){
+                SerialBT.print("error= ");
+                SerialBT.println(codeError);
+            }
+            else if(line.indexOf("name=") != -1){
+                indexWord = line.indexOf("name=");
+                fileNameBt = line.substring(5);
+                if (fileNameBt.charAt(0) == ' '){
+                    fileNameBt.remove(0,1);
+                }
+                Serial.print("Nombre del archivo: ");
+                Serial.println(fileNameBt);      
+                while(true){
+                    SerialBT.println("ok");
+                    codeError = readLine(line);
+                    if (codeError != 0){
+                        SerialBT.print("error= ");
+                        SerialBT.println(codeError);
+                        break;
+                    }                
+                    if (line.indexOf("bytes=") != -1 ){
+                        indexWord = line.indexOf("bytes=");
+                        bytesToRead = line.substring(6).toInt();
+                        if (bytesToRead == 0){
+                          SerialBT.println("error= -2"); //Numero de bytes incorrecto
+                          break; //
+                        }
+                        SerialBT.println("ok");
+                        codeError = readBt(dataBt, bytesToRead);
+                        if (codeError != 0){
+                            SerialBT.print("error= ");
+                            SerialBT.println(codeError);
+                            break;
+                        }
+                        for (int i = 0; i < bytesToRead; i++){
+                            Serial.print(char(dataBt[i]));
+                        }
+                    }
+                    else if(line.equals("done")){
+                        SerialBT.println("ok");
+                        Serial.println("guardado en memoria");
+                        break;
+                    }
+                    else{
+                        SerialBT.println("error= -1"); //Comando incorrecto
+                        break;
                     }
                 }
-                else if(line.equals("done")){
-                    serial.println("guardado en memoria");
-                    break;
-                }
+            }
+            else {
+                SerialBT.println("error= -4"); //No se proporciono nombre
             }
         }
     }
     delay(1000);
 }
 
+unsigned long timeOutBt = 30000;
 
 int readLine(String& line){
     line = "";
-    int byte = -1;
-    while(char(byte) != '\n'){
-        if (SerialBT.available()) {
-            byte = SerialBT.read();
-            line.concat(char(byte));
+    int indexRemove;
+    int byteRecived = -1;
+    unsigned long tInit = millis();
+    while(char(byteRecived) != '\n'){
+        if (millis() - tInit < timeOutBt){
+            if (SerialBT.available()) {
+                byteRecived = SerialBT.read();
+                line.concat(char(byteRecived));
+            }        
+        }
+        else{
+            return -3; //timeOut
         }
     }
-    serial.print(line);
+    indexRemove = line.indexOf('\r');
+    if (indexRemove != -1){
+      line.remove(indexRemove, 1);
+    }
+    indexRemove = line.indexOf('\n');
+    line.remove(indexRemove, 1);
+    Serial.println(line);
     return 0;
 }
 
-int readBt(uint8_t dataBt, int bytesToRead){
+int readBt(uint8_t dataBt[], int bytesToRead){
     int i = 0;
+    unsigned long tInit = millis();
     while(i < bytesToRead){
-        if (SerialBT.available()) { 
-            dataBt[i] = SerialBT.read();
-            i += 1;
+        if (millis() - tInit < timeOutBt){
+            if (SerialBT.available()) { 
+                dataBt[i] = SerialBT.read();
+                i += 1;
+            }       
         }
+        else{
+            return -3; //timeOut
+        }
+        
+    }
+    while (SerialBT.available()) { 
+        SerialBT.read();
     }
     return 0;
 }
